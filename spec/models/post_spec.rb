b1c9ba0e042f9dc433a22f2cd6084bfd0311e077
Newton <anthpaul0@gmail.com>
2015-03-02 16:01:33 -0600

    post factory

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do 
+		@post = FactoryGirl.create(:post)
+	end
+
+	it "has a valid factory" do 
+		expect(@post).to be_valid
+	end
+  
 end
