9bf835b44e6d9a85036e3b44481f671981a3f712
Newton <anthpaul0@gmail.com>
2015-03-03 17:25:46 -0600

    User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
+
+    email                     "user@example.com"
+    password                  "foobarextra"
+    password_confirmation     "foobarextra"
     
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user_attr = FactoryGirl.attributes_for(:user)
+  end
+
+  it 'should create a new instance of user with valid attributes' do
+    @user = User.create!(@user_attr)
+  end
 end
