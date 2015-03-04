17f67671e5f36d7acdc1e01b76d9c50c3f72a01c
Newton <anthpaul0@gmail.com>
2015-03-04 14:23:48 -0600

    Deleted some integration tests, need move over to views

--- spec/features/posts_index_spec.rb
+++ spec/features/posts_index_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe 'the posts index', type: :feature do
 
-  post = FactoryGirl.create(:post)
+  # post = FactoryGirl.create(:post)
 
   before(:each) do
     visit posts_path
   end
 
+  #todo
+  #probably elete this too and move it over to 
+  #a view spec
   it 'has a form to create a new post' do
     expect(page).to have_button('Create Post')
   end
@@ -26,17 +29,10 @@ describe 'the posts index', type: :feature do
     page.fill_in('Title', with: 'foobar')
     page.fill_in('Body', with: 'barfoho')
     page.click_button('Create Post')
+    
     expect(current_path).to eq(posts_path)
     expect(page).not_to have_content('error')
     expect(page).to have_content('barfoho')
   end
 
-  it 'sends user to post page when title is clicked' do
-    expect(page).to have_link(post.title, href: post_path(post))
-
-  end
-
-  xit 'lists shows the user who created the post' do
-    expect(page).to have_content(post.user)
-  end
 end
