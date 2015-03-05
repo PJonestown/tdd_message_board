c4ecef61a02a417c52f0c7f3c1d4e84f6d6cee3a
Newton <anthpaul0@gmail.com>
2015-03-05 13:34:49 -0600

    When logged in it shows the correct links

--- spec/features/posts_view_spec.rb
+++ spec/features/posts_view_spec.rb
@@ -14,4 +14,21 @@ describe 'the root view', type: :feature do
     end
 
   end
+
+  describe 'when logged in' do
+
+    before(:each) do
+      click_link('Sign up')
+      page.fill_in('Email', with: 'yes@gmail.com')
+      page.fill_in('Password', with: 'foobarfoo')
+      page.fill_in('Password confirmation', with: 'foobarfoo')
+      page.click_button('Sign up')
+    end
+
+    it 'shows the correct links' do
+      expect(page).to have_link('Sign out')
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
 end
