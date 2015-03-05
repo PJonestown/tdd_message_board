e1e7bd52dd7a8d47d81a62a583572af9607a47a8
Newton <anthpaul0@gmail.com>
2015-03-05 12:53:35 -0600

    delete post without ownership integration test

--- spec/features/post_delete_process_spec.rb
+++ spec/features/post_delete_process_spec.rb
@@ -19,12 +19,27 @@ describe 'deleting a post', type: :feature do
     it 'should delete the post' do
       visit root_path
       page.click_link('Destroy')
-
-
       expect(page).to have_content('Post was successfully destroyed.')
+    end
+  end
 
+  describe 'without ownership' do
+    
+    it 'should show an error and not delete the post' do
+
+      click_link('Sign out')
+      # expect(page).to eq(root_path)
+      visit new_user_registration_path
+      page.fill_in('Email', with: 'second@gmail.com')
+      page.fill_in('Password', with: 'foobarfoo')
+      page.fill_in('Password confirmation', with: 'foobarfoo')
+      page.click_button('Sign up')
+
+      #expect(page).to eq(root_path)
+      page.click_link('Destroy')
+      expect(page).to have_content('A title')
+      expect(page).to have_content("You don't have permission to do this")
 
     end
-
   end
 end
