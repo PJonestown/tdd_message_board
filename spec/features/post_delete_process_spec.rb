0b7811c3a910533ec07562daef9283e036f3e673
Newton <anthpaul0@gmail.com>
2015-03-05 13:06:02 -0600

    Added sign up link

--- spec/features/post_delete_process_spec.rb
+++ spec/features/post_delete_process_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe 'deleting a post', type: :feature do
 
   before(:each) do
-    visit new_user_registration_path
+    visit root_path
+    page.click_link('Sign up')
     page.fill_in('Email', with: 'first@gmail.com')
     page.fill_in('Password', with: 'foobarfoo')
     page.fill_in('Password confirmation', with: 'foobarfoo')
     page.click_button('Sign up')
+
     expect(current_path).to eq(root_path)
     page.fill_in('Title', with: 'A title')
     page.fill_in('Body', with:  'A body')
@@ -28,14 +30,13 @@ describe 'deleting a post', type: :feature do
     it 'should show an error and not delete the post' do
 
       click_link('Sign out')
-      # expect(page).to eq(root_path)
-      visit new_user_registration_path
+      
+      click_link('Sign up') 
       page.fill_in('Email', with: 'second@gmail.com')
       page.fill_in('Password', with: 'foobarfoo')
       page.fill_in('Password confirmation', with: 'foobarfoo')
       page.click_button('Sign up')
 
-      #expect(page).to eq(root_path)
       page.click_link('Destroy')
       expect(page).to have_content('A title')
       expect(page).to have_content("You don't have permission to do this")
